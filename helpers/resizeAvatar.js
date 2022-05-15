ca4a42f0f053aa4a92e4e2760899d49dcca1cbfd
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-05-15 18:52:15 +0300

    refactor extension image

--- helpers/resizeAvatar.js
+++ helpers/resizeAvatar.js
@@ -3,15 +3,20 @@ const createError = require("http-errors");
 const Jimp = require("jimp");
 const { v4: uuidv4 } = require("uuid");
 
+const RESIZE_HEIGHT = 250;
+const RESIZE_WIDTH = 250;
+const RESIZE_QUALITY = 80;
+
 const resizeAvatar = async ({ path }) => {
   const avatarPath = await Jimp.read(path)
     .then((lenna) => {
-      const filePath = path.split(".");
-      const fileType = filePath[filePath.length - 1].toLowerCase();
+      const fileExtension = lenna.getExtension();
+      const fileName = `${uuidv4()}.${fileExtension}`;
 
-      const fileName = `${uuidv4()}.${fileType}`;
-
-      lenna.resize(250, 250).quality(80).write(`./public/avatars/${fileName}`);
+      lenna
+        .resize(RESIZE_HEIGHT, RESIZE_WIDTH)
+        .quality(RESIZE_QUALITY)
+        .write(`./public/avatars/${fileName}`);
 
       fs.unlink(path);
 
